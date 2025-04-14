81a895a31d1af6b52e22ddfd3deb99d21ea2c0e0
benofcoding <166450432+benofcoding@users.noreply.github.com>
2025-04-14 22:36:55 +1200

    added ids to the il categories

--- speedruncom_scrapper.py
+++ speedruncom_scrapper.py
@@ -30,14 +30,24 @@ def run_query_insert(query, values):
 
     cursor.close()
     conn.close()
+def run_query_update(query):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(query)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
 
 game = api.search(srcomapi.datatypes.Game, {"name": "Celeste"})[0]
 game_id = game.id
 
 
 
-
 def insert_levels():
+
 
     levels = api.get(f'games/{game_id}/levels')
     level = []
@@ -49,6 +59,28 @@ def insert_levels():
         while id in ids:
             id = generate_id()
         run_query_insert('INSERT INTO Level (level_id, name) VALUES (?, ?)', (id, level))
-    
 
-insert_levels()
+
+
+def id_il_categories():
+    categories_temp = run_query_select('SELECT Il_category.name FROM Il_category')
+    categories = []
+
+    for i in categories_temp:
+        categories.append(i[0])
+    print(categories)
+
+
+    for i in categories:
+        id = generate_id()
+        ids_temp = run_query_select('SELECT Il_category.il_category_id FROM Il_category')
+        ids = []
+        for j in ids_temp:
+            ids.append(j[0])
+        while id in ids:
+            id = generate_id()
+        query = f"UPDATE Il_category SET il_category_id = '{id}' WHERE Il_category.name = '{i}'"
+        print(query)
+        run_query_update(query)
+
+id_il_categories()
