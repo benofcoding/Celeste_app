5015771514eddcbd76451a475a88a1794652e7a8
benofcoding <166450432+benofcoding@users.noreply.github.com>
2025-04-14 21:22:33 +1200

    scrubbed all the levels for celeste and added them to the database, i couldve done this manually however this just lets me learn how to do this more

--- speedruncom_scrapper.py
+++ speedruncom_scrapper.py
@@ -9,32 +9,46 @@ def generate_id():
     for i in range(8):
         id += random.choices('0123456789abcdef', k=1)[0]
     return id
+def run_query_select(query):
 
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
 
-print(generate_id())
+    return rows
+def run_query_insert(query, values):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(query, values)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
 
 game = api.search(srcomapi.datatypes.Game, {"name": "Celeste"})[0]
 game_id = game.id
 
-def run_query(query):
-    try:
-        conn = sqlite3.connect('DB NAME')
-        cursor = conn.cursor()
-        
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        
-        cursor.close()
-        conn.close()
 
-        return rows
-    except sqlite3.Error as e:
-        return None
 
-def get_levels():
+
+def insert_levels():
 
     levels = api.get(f'games/{game_id}/levels')
     level = []
 
     for i in levels:
-        level.append(i['name'])
+        level = i['name']
+        ids = run_query_select('SELECT Level.level_id FROM Level')
+        id = generate_id()
+        while id in ids:
+            id = generate_id()
+        run_query_insert('INSERT INTO Level (level_id, name) VALUES (?, ?)', (id, level))
+    
+
+insert_levels()
