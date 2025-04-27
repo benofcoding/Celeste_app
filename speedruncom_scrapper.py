3a394e8a098cb7e777bf0b6c50928404161ce793
benofcoding <166450432+benofcoding@users.noreply.github.com>
2025-04-28 11:39:59 +1200

    did some stuff and added the main python file

--- speedruncom_scrapper.py
+++ speedruncom_scrapper.py
@@ -1,6 +1,7 @@
 import srcomapi, srcomapi.datatypes as dt
 import sqlite3
 import random
+import time
 api = srcomapi.SpeedrunCom(); api.debug = 1
 
 
@@ -79,7 +80,6 @@ def id_il_categories():
         query = f"UPDATE Il_category SET il_category_id = '{id}' WHERE Il_category.name = '{i}'"
         print(query)
         run_query_update(query)
-
 def individual_levels():
     category_ids = ['3eadb0eb','f49213bb','061a9cce','75f87514','1a682a2a','40ce5c88']
     level_ids = ['75cdc954','91dbb1b1','f993ae32','2abf90d5','68000667','48762587','12768c3f','b1798bf3']
@@ -95,8 +95,131 @@ def individual_levels():
                 id = generate_id()
             
             run_query_insert('INSERT INTO Individual_level (il_id, level_id, il_category_id) VALUES (?, ?, ?)', (id, j, i))
-            
+def get_categories():
+
+    categories = [i['name'] for i in api.get(f'games/{game_id}/categories')]
+    
+    for i in categories:
+        id = generate_id()
+        while id in [i[0] for i in run_query_select('SELECT Fullgame_category.fullgame_category_id FROM Fullgame_category')]:
+            id = generate_id()
+        
+        run_query_insert('INSERT INTO Fullgame_category (fullgame_category_id, name) VALUES (?, ?)', (id, i))
+def get_platforms():
+    platform_ids = [i for i in api.get(f'games/{game_id}')['platforms']]
+    platforms = []
+
+    for i in platform_ids:
+        platforms.append(api.get(f'platforms/{i}')['name'])
+
+    for i in platforms:
+        id = generate_id()
+        while id in [i[0] for i in run_query_select('SELECT Platform.platform_id FROM Platform')]:
+            id = generate_id()
+
+        run_query_insert('INSERT INTO Platform (platform_id, name) VALUES (?, ?)', (id, i))
+def get_users():
+    platform_ids = [i for i in api.get(f'games/{game_id}')['platforms']]
+    levels = [i['id'] for i in api.get(f'games/{game_id}/levels')]
+    print(levels)
+    clear = platform_ids[1]
+    del platform_ids[1]
+    category_ids = [i['id'] for i in api.get(f'games/{game_id}/categories')]
+    print(category_ids)
+    print(platform_ids)
+    runs = []
+
+    for j in category_ids:
+        for i in platform_ids:
+            offset = 0
+            while (offset < 10000):
+
+                if len(a := api.get(f'runs?game={game_id}&max=200&offset={offset}&orderby=date&direction=desc&platform={i}&category={j}')) == 0:
+                    break
+                else:
+                    runs += a
+                offset += 200
+                print(len(runs))
+
+            offset = 0
+            while (offset < 10000):
+
+                if len(a := api.get(f'runs?game={game_id}&max=200&offset={offset}&orderby=date&direction=asc&platform={i}&category={j}')) == 0:
+                    break
+                else:
+                    runs += a
+                offset += 200
+                print(len(runs))
+
+    for j in levels:
+        for i in platform_ids:
+            offset = 0
+            while (offset < 10000):
+                if len(a := api.get(f'runs?game={game_id}&max=200&offset={offset}&orderby=date&direction=desc&platform={i}&category={clear}&level={j}')) == 0:
+                    break
+                else:
+                    runs += a
+                offset += 200
+                print(len(runs))
+
+            offset = 0
+            while (offset < 10000):
+                if len(a := api.get(f'runs?game={game_id}&max=200&offset={offset}&orderby=date&direction=asc&platform={i}&category={clear}&level={j}')) == 0:
+                    break
+                else:
+                    runs += a
+                offset += 200
+                print(len(runs))
 
 
 
-print(generate_id(), generate_id())
+
+    player_ids = set()
+    player_names = set()
+
+    for i in runs:
+        if len(i['players']) != 0:
+            if i['players'][0]['rel'] == 'guest':
+                player_names.add(i['players'][0]['name'])
+            else:
+                player_ids.add(i['players'][0]['id'])
+
+    print(player_names)
+def get_users2():
+
+
+
+    with open('ids.txt', 'r') as f:
+        input_str = f.read()
+
+    a = [s.strip().strip("'") for s in input_str.split(',')]
+
+
+
+
+    countbig = 85
+    count = 0
+    player_names = set(['Xylex', 'RA.', 'NinjaPear22', 'Witchs_Hex', 'Podz', 'yarniapple', 'Fabi000n', 'TeDeMos', 'Beeb_', 'Ac_143', 'finn_', 'OkaiOkai', 'Ricka', 'AtomicHex', 'Lifuu', 'Frostysnowman', 'Norikokonut', 'B3traya1', 'shredberg', 'JeddOrAlive'])
+
+
+
+    while countbig < 90:
+        count = 0
+        print(countbig*5)
+        player_names = set()
+        while count < 100:
+            print(count)
+            c = api.get(f'users/{a[count+(countbig*100)]}')['names']['international']
+            time.sleep(0.61)
+            count += 1
+            id = generate_id()
+            while id in [j[0] for j in run_query_select('SELECT Player.player_id FROM Player')]:
+                id = generate_id()
+            run_query_insert('INSERT INTO Player (player_id, name) VALUES (?, ?)', (id, c))
+
+        countbig += 1
+
+
+
+
+
