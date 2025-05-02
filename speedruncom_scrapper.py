4048a8f28e760b66791318713f21b0095937347c
benofcoding <166450432+benofcoding@users.noreply.github.com>
2025-05-02 13:29:38 +1200

    SAVING THE DATABASE IN CASE OF EMERGANCY

--- speedruncom_scrapper.py
+++ speedruncom_scrapper.py
@@ -2,9 +2,37 @@ import srcomapi, srcomapi.datatypes as dt
 import sqlite3
 import random
 import time
+import datetime
 api = srcomapi.SpeedrunCom(); api.debug = 1
 
 
+fullgame = { 
+'7kjpl1gk':'Any%',
+'jdz8oprd':'All Red Berries',
+'zdn0m372':'True Ending',
+'q2517ggd':'All Cassettes',
+'q2550mw2':'Bny%',
+'xd1718wd':'All Hearts',
+'xk9ry6xk':'100%',
+'jdrvgy02':'202 Berries',
+'z27rpe5d':'All Chapters',
+'zd365jyd':'All A-Sides',
+'zd36mqyd':'All B-Sides',
+'xd1ex5r2':'All C-Sides'}
+
+platforms = {'nzelkr6q': 'PlayStation 4', 'o7e2mx6w': 'Xbox One', '8gej2n93': 'PC', '7m6ylw9p': 'Switch', '4p9z0r6r': 'Xbox One X', 'o064j163': 'Xbox One S', 'o064z1e3': 'Google Stadia', '4p9zjrer': 'PlayStation 5', 'nzelyv9q': 'Xbox Series X', 'o7e2xj9w': 'Xbox Series S'}
+
+levels = {'ywe5zq7w': 'Forsaken City', '69z2m8g9': 'Old Site', 'r9g4k7p9': 'Celestial Resort', 'o9x7mxpd': 'Golden Ridge', '4955vm39': 'Mirror Temple', 'rdq76n29': 'Reflection', '5d746x6d': 'The Summit', 'kwjzo679': 'Core', '5wknr4qw': 'Farewell'}
+
+categorys = {'7dgr144k': 'Clear', 'mkezwq9k': 'Collectibles', '5dw5q7gd': 'B-Side', 'wk67rved': 'C-Side'}
+
+
+
+def time_since_1980(date_str):
+    dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+    ref_date = datetime(1980, 1, 1)
+    delta = dt - ref_date
+    return delta.total_seconds()
 def generate_id():
     id = ''
     for i in range(8):
@@ -44,6 +72,11 @@ def run_query_update(query):
 
 game = api.search(srcomapi.datatypes.Game, {"name": "Celeste"})[0]
 game_id = game.id
+
+categories = api.get(f'games/{game_id}/categories')
+
+
+
 
 
 
@@ -222,4 +255,152 @@ def get_users2():
 
 
 
+
+
+
+
+
+
+
+
+
+
+# for j in fullgame:
+#     runs = []
+#     pagnation = api.get(f'leaderboards/{game_id}/category/{j}')
+#     for i in pagnation['runs']:
+#         runs.append(i['run'])
+#         print(len(runs))
+
+#     for i in runs:
+#         verified = True
+#         if 'id' in i['players'][0]:
+#             user = i['players'][0]['id']
+#             if (a := len(run_query_select(f"SELECT name FROM Player WHERE name = '{user}'"))) == 0:
+#                 run_query_insert('INSERT INTO Player (player_id, name) VALUES (?, ?)', (generate_id(), user))
+#             run_idd = generate_id()
+#             verifier_idd = run_query_select(f"SELECT verifier_id FROM Verifier JOIN Player On Verifier.player_id = Player.player_id WHERE Player.name = '{i['status']['examiner']}'")
+#             if len(verifier_idd) == 0:
+#                 verified = False
+#             else:
+#                 verifier_idd = verifier_idd[0][0]
+#             user_idd = run_query_select(f"SELECT player_id FROM Player WHERE name = '{i['players'][0]['id']}'")[0][0]
+#             fullgame_category_idd = run_query_select(f"SELECT fullgame_category_id FROM Fullgame_category WHERE name = '{fullgame[j]}'")[0][0]
+#             platform_idd = run_query_select(f"SELECT platform_id FROM Platform WHERE name = '{platforms[i['system']['platform']]}'")[0][0]
+#             timee = i['times']['primary_t']
+#             datesubmittedd = time_since_1980(i['submitted'])
+#             if 'links' in i['videos']:
+#                 linkk = i['videos']['links'][0]['uri']
+#             else:
+#                 linkk = False
+
+#             if linkk:
+#                 if verified:
+#                     run_query_insert('INSERT INTO Run (run_id, verifier_id, player_id, fullgame_category_id, platform_id, time, date_submitted, video_link) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (generate_id(), verifier_idd, user_idd, fullgame_category_idd, platform_idd, timee, datesubmittedd, linkk))
+#                 else:
+#                     run_query_insert('INSERT INTO Run (run_id, player_id, fullgame_category_id, platform_id, time, date_submitted, video_link) VALUES (?, ?, ?, ?, ?, ?, ?)', (generate_id(), user_idd, fullgame_category_idd, platform_idd, timee, datesubmittedd, linkk))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in fullgame:
+#     pagnation = api.get(f'leaderboards/{game_id}/category/{i}?embed=players')
+#     players = pagnation['players']['data']
+#     for i in players:
+#         print(i)
+#         if i['rel'] == 'guest':
+#             continue
+#         player = i['id']
+#         if len(run_query_select(f'SELECT name FROM Player WHERE name = "{player}"')) == 0:
+#             run_query_insert(f'INSERT INTO Player (player_id, name) VALUES (?, ?)', (generate_id(), player))
+
+# for i in levels:
+#     for j in categorys:
+#         pagnation = api.get(f'leaderboards/{game_id}/level/{i}/{j}?embed=players')
+#         players = pagnation['players']['data']
+#         for v in players:
+#             print(v)
+#             if v['rel'] == 'guest':
+#                 continue
+#             player = v['id']
+#             if len(run_query_select(f'SELECT name FROM Player WHERE name = "{player}"')) == 0:
+#                 run_query_insert(f'INSERT INTO Player (player_id, name) VALUES (?, ?)', (generate_id(), player))
+
+users = run_query_select('SELECT name FROM Player')
+for i in users:
+    print(i[0])
+    i = i[0]
+    runs = api.get(f'runs?user={i}')
 
